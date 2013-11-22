0b2d1aa54eb3344ba72a85a8cf52a44dbc579b0b
Alvaro Lopez Ortega <alvaro@alobbs.com>
2013-11-22 09:24:19 -0600

    Quick update

--- msg_my_staff.py
+++ msg_my_staff.py
@@ -5,17 +5,19 @@ TO = [('Daniel Radez', 'dradez@redhat.com'),
       ('Julie Pichon', 'jpichon@redhat.com'),
       ('Martin Magr', 'mmagr@redhat.com'),
       ('Matthias Runge', 'mrunge@redhat.com'),
-      ('Ondrej Slampa', 'oslampa@redhat.com'),
+      ('Francesco Vollero', 'fvollero@redhat.com'),
       ('William Foster', 'wfoster@redhat.com')]
 CC = []
 
 BODY = """\
 Hi folks!
 
-This is just a friendly reminder that I expect your weekly status reports by tomorrow.
+This is a friendly reminder about your weekly status report.
 
 Please, reply to this message with your status update, optionally copying your team's mailing list.
 
-Thanks,
+Thank you very much.
+
+All the best,
 Alvaro
 """
